75463fb29df8dc3244ac44263f0695646a754085
John Watson <john.watson1@defra.gov.uk>
2025-03-22 12:39:09 +0000

    Rename variable

--- src/plugins/headers.js
+++ src/plugins/headers.js
@@ -1,7 +1,7 @@
 const plugin = {
   plugin: {
     name: 'headers',
-    register: (server, options) => {
+    register: (server, _options) => {
       server.ext('onPreResponse', (request, h) => {
         const response = request.response
 
