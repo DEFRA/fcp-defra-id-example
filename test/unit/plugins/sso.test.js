8756406117f8ebab23f7a4fc2e48c47f90b7bf19
John Watson <john.watson1@defra.gov.uk>
2025-03-28 11:50:50 +0000

    Add SSO test

---
+++ test/unit/plugins/sso.test.js
@@ -0,0 +1,15 @@
+import sso from '../../../src/plugins/sso.js'
+
+describe('sso', () => {
+  test('should return an object', () => {
+    expect(sso).toBeInstanceOf(Object)
+  })
+
+  test('should name the plugin', () => {
+    expect(sso.plugin.name).toBe('sso')
+  })
+
+  test('should have a register function', () => {
+    expect(sso.plugin.register).toBeInstanceOf(Function)
+  })
+})
