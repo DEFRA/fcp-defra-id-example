7d9ec6fe1370cd7909fbd315ebbcc3c4e21cc1ac
John Watson <john.watson1@defra.gov.uk>
2025-03-30 20:08:00 +0100

    Add assets route tests

---
+++ test/integration/routes/assets.test.js
@@ -0,0 +1,37 @@
+import { jest } from '@jest/globals'
+import '../helpers/setup-server-mocks.js'
+
+const { createServer } = await import('../../../src/server.js')
+
+let server
+
+describe('assets route', () => {
+  beforeAll(async () => {
+    jest.clearAllMocks()
+
+    server = await createServer()
+    await server.initialize()
+  })
+
+  afterAll(async () => {
+    if (server) {
+      await server.stop()
+    }
+  })
+
+  test('GET /assets returns local asset', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/assets/govuk-frontend.min.css'
+    })
+    expect(response.statusCode).toBe(200)
+  })
+
+  test('GET /assets returns node_modules asset', async () => {
+    const response = await server.inject({
+      method: 'GET',
+      url: '/assets/images/favicon.ico'
+    })
+    expect(response.statusCode).toBe(200)
+  })
+})
