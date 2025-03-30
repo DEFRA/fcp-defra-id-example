2df94c693769910dc241ccdae750e0df7c5a9267
John Watson <john.watson1@defra.gov.uk>
2025-03-30 21:45:44 +0100

    Add tests for auth sign-in routes with OIDC redirection

--- test/integration/routes/auth.test.js
+++ test/integration/routes/auth.test.js
@@ -1,6 +1,29 @@
 import { jest } from '@jest/globals'
 import { mockOidcConfig } from '../helpers/setup-server-mocks.js'
 
+const mockVerifyToken = jest.fn()
+jest.unstable_mockModule('../../../src/auth/verify-token', async () => ({
+  verifyToken: mockVerifyToken
+}))
+
+const mockGetPermissions = jest.fn()
+jest.unstable_mockModule('../../../src/auth/get-permissions.js', async () => ({
+  getPermissions: mockGetPermissions
+}))
+
+const credentials = {
+  profile: {
+    sessionId: 'session-id',
+    crn: '1234567890',
+    organisationId: '1234567'
+  },
+  token: 'DEFRA-ID-JWT',
+  refreshToken: 'DEFRA-ID-REFRESH-TOKEN'
+}
+
+const role = 'Farmer'
+const scope = ['user']
+
 const { createServer } = await import('../../../src/server.js')
 
 let server
@@ -8,6 +31,8 @@ let server
 describe('auth routes', () => {
   beforeAll(async () => {
     jest.clearAllMocks()
+
+    mockGetPermissions.mockResolvedValue({ role, scope })
 
     server = await createServer()
     await server.initialize()
@@ -26,22 +51,65 @@ describe('auth routes', () => {
         url: '/auth/sign-in',
         auth: {
           strategy: 'defra-id',
-          credentials: {
-            sessionId: 'session-id'
-          }
+          credentials
         }
       })
       expect(response.statusCode).toBe(302)
       expect(response.headers.location).toBe('/home')
     })
 
-    test('redirects to oidc sign in page if unauthenticated', async () => {
+    test('redirects to oidc sign in if unauthenticated', async () => {
       const response = await server.inject({
         method: 'GET',
         url: '/auth/sign-in'
       })
+      const redirect = new URL(response.headers.location)
+      const params = new URLSearchParams(redirect.search)
+      expect(response.statusCode).toBe(302)
+      expect(redirect.origin).toBe('https://oidc.example.com')
+      expect(redirect.pathname).toBe('/authorize')
+      expect(params.get('serviceId')).toBe(process.env.DEFRA_ID_SERVICE_ID)
+      expect(params.get('p')).toBe(process.env.DEFRA_ID_POLICY)
+      expect(params.get('response_mode')).toBe('query')
+      expect(params.get('client_id')).toBe(process.env.DEFRA_ID_CLIENT_ID)
+      expect(params.get('response_type')).toBe('code')
+      expect(params.get('redirect_uri')).toBe(process.env.DEFRA_ID_REDIRECT_URL)
+      expect(params.get('state')).toBeDefined()
+      expect(params.get('scope')).toBe(`openid offline_access ${process.env.DEFRA_ID_CLIENT_ID}`)
+    })
+  })
+
+  describe('GET /auth/sign-in-oidc', () => {
+    test('redirects to /home if authenticated and no redirect path in session', async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/auth/sign-in-oidc',
+        auth: {
+          strategy: 'defra-id',
+          credentials
+        }
+      })
+      expect(response.statusCode).toBe(302)
+      expect(response.headers.location).toBe('/home')
+    })
+
+    test('redirects to oidc sign in page if unauthenticated and no redirect params', async () => {
+      const response = await server.inject({
+        method: 'GET',
+        url: '/auth/sign-in-oidc'
+      })
       expect(response.statusCode).toBe(302)
       expect(response.headers.location.startsWith(mockOidcConfig.authorization_endpoint)).toBe(true)
     })
+
+    // test('returns unauthorised view if not authenticated but redirect params', async () => {
+    //   const response = await server.inject({
+    //     method: 'GET',
+    //     url: '/auth/sign-in-oidc?state=state&code=code'
+    //   })
+    //   console.log(response.payload)
+    //   expect(response.statusCode).toBe(200)
+    //   expect(response.request.response.source.template).toBe('unauthorised')
+    // })
   })
 })
