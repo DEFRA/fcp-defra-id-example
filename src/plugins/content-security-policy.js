0d004c9bdd563458cf3a6a5fba8c9a6fd2a4194e
John Watson <john.watson1@defra.gov.uk>
2025-07-30 18:01:09 +0100

    Add GOV.UK frontend hash comment

--- src/plugins/content-security-policy.js
+++ src/plugins/content-security-policy.js
@@ -1,5 +1,7 @@
 import Blankie from 'blankie'
 
+// Hash 'sha256-GUQ5ad8JK5KmEWmROf3LZd9ge94daqNvd8xy9YS1iDw=' is to support a GOV.UK frontend script bundled within Nunjucks macros
+// https://frontend.design-system.service.gov.uk/import-javascript/#if-our-inline-javascript-snippet-is-blocked-by-a-content-security-policy
 export default {
   plugin: Blankie,
   options: {
